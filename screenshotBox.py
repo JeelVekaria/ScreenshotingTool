05286f8a99080747a11f1432456c4aa97f8c2edc
Jeel-Vekaria <jeelvekaria@gmail.com>
2025-03-28 13:33:49 -0400

    Get mouse position

---
+++ screenshotBox.py
@@ -0,0 +1,12 @@
+from tkinter import * # GUI
+import pyautogui # mouse events
+
+print('Press Ctrl-C to quit.')
+try:
+    while True:
+        x, y = pyautogui.position()
+        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+        print(positionStr, end='')
+        print('\b' * len(positionStr), end='', flush=True)
+except KeyboardInterrupt:
+    print('trackCursor.py stoppped.\n')
