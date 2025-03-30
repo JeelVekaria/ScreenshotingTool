ecc4602fffe7c1fe444e38b066f9ab49cf21e4be
Jeel-Vekaria <jeelvekaria@gmail.com>
2025-03-30 11:12:35 -0400

    Add visual indicators of screenshotting region

--- screenshotBox.py
+++ screenshotBox.py
@@ -4,6 +4,10 @@ import pyautogui
 from datetime import datetime
 import ctypes
 
+
+def endApplication(event):
+    root.destroy()
+
 def validate_input(value):
     return value.isdigit() or value == ""
 
@@ -15,21 +19,45 @@ def validate_coordinates(x1, x2, y1, y2):
     if(y1<0 or y1>winHeight):
         y1Label.configure(bg='red')
         valid=False
-    if(x2>winWidth or x2<=x1):
+    if(x2<0 or x2>winWidth or x1==x2):
         x2Label.configure(bg='red')
         valid=False
-    if(y2>winHeight or y2<=y1):
+    if(y2<0 or y2>winHeight or y1==y2):
         y2Label.configure(bg='red')
         valid=False
     return valid
 
-def update_coords():
+def resetLabelBackground():
+    x1Label.configure(bg=originalBackground)
+    y1Label.configure(bg=originalBackground)
+    x2Label.configure(bg=originalBackground)
+    y2Label.configure(bg=originalBackground)
+
+def updateCoords():
     x, y = pyautogui.position()
     liveCoords.config(text=f"X: {x}, Y: {y}")
-    firstCoordsFrame.after(100, update_coords)
+    miscFrame.after(100, updateCoords)
 
-def submit():
-    today=str(datetime.today().strftime('%Y-%m-%d %H:%M:%S')).replace(":","_").replace(" ","_")
+
+# event used to detect keypress change despite not using in code block
+def setStartingCoords(event = Event):
+    x, y = pyautogui.position()
+    x1Coord.set(value=x)
+    y1Coord.set(value=y)
+    createWindowForPlus('start')
+
+def setEndingCoords(event = Event):
+    x, y = pyautogui.position()
+    x2Coord.set(value=x)
+    y2Coord.set(value=y)
+    createWindowForPlus('end')
+
+def createPlusSign(canvas, x, y, size, color):
+    canvas.create_line(x - size, y, x + size, y, fill=color, width=2)
+    canvas.create_line(x, y - size, x, y + size, fill=color, width=2)
+
+def snapClicked(event=Event):
+    # gets coords, if empty set to opposite corners of screen
     x1 = int(x1Coord.get() if x1Coord.get() != '' else 0) 
     x2 = int(x2Coord.get() if x2Coord.get() != '' else winWidth) 
     y1 = int(y1Coord.get() if y1Coord.get() != '' else 0) 
@@ -39,21 +67,76 @@ def submit():
         return
     
     # Hides window to take screenshot without this application in the way
+    hideAllWindows()
+    screenshot(x1=x1, y1=y1, x2=x2, y2=y2)
+    showAllWindows()
+
+def hideAllWindows():
     root.withdraw()
-    screenshot = ImageGrab.grab(bbox=(x1, y1, x2, y2), include_layered_windows=False, all_screens=True)
-    screenshot.save("Snap_"+today+".png", format="PNG")
+    startPlus.withdraw()
+    endPlus.withdraw()
+
+def showAllWindows():
     root.deiconify()
+    startPlus.deiconify()
+    endPlus.deiconify()
+
+def screenshot(x1, y1, x2, y2):
+    resetLabelBackground()
+    today=str(datetime.today().strftime('%Y-%m-%d %H:%M:%S')).replace(":","_").replace(" ","_")
+    screenshot = ImageGrab.grab(bbox=(x1 if x1<x2 else x2, y1, x2 if x1<x2 else x1, y2), include_layered_windows=False, all_screens=True)
+    screenshot.save("Snap_"+today+".png", format="PNG")
+
+def createWindowForPlus(position, newX=None, newY=None):
+    # sets window to where user inputs number
+    if(newX and newY):
+        x, y = int(newX.get() or 0), int(newY.get() or 0)
+    # sets window based on cursor location from shortcuts <F1>, <F2>
+    else:
+        x, y = pyautogui.position()
+    xPos = x-10
+    yPos = y-12
+    match position:
+        case 'start':
+            startPlus.deiconify()
+            startPlus.geometry("30x30+%d+%d" % (xPos, yPos))
+            return
+        case 'end':
+            endPlus.deiconify()
+            endPlus.geometry("30x30+%d+%d" % (xPos, yPos))
+            return
 
 # Keeps window on top
 root = Tk()
 root.resizable(0,0)
+root.focus_force()
 root.attributes(toolwindow=1)
 root.title("Snap Screen")
 root.attributes('-topmost', True)
 
-# Get window dimensions
+# Get window dimensions & color
 user32 = ctypes.windll.user32
 winWidth, winHeight = user32.GetSystemMetrics(78), user32.GetSystemMetrics(79)
+originalBackground = root.cget("background")
+
+# Window for plus signs
+startPlus = Toplevel()
+startPlus.attributes('-transparentcolor', startPlus['bg'])
+startPlus.overrideredirect(True)
+startCanvas = Canvas(startPlus, width=12, height=12)
+startCanvas.pack()
+startCanvas.place(x=1,y=1)
+createPlusSign(startCanvas, x=10, y=10, size=4, color="green")
+startPlus.withdraw()
+
+endPlus = Toplevel()
+endPlus.attributes('-transparentcolor', endPlus['bg'])
+endPlus.overrideredirect(True)
+endCanvas = Canvas(endPlus, width=12, height=12)
+endCanvas.pack()
+endCanvas.place(x=1,y=1)
+createPlusSign(endCanvas, x=10, y=10, size=4, color="red")
+endPlus.withdraw()
 
 # Frame partitions root window into blocks/containers
 firstCoordsFrame = Frame(root)
@@ -63,6 +146,9 @@ miscFrame = Frame(root)
 # Validation for input
 validate_func = root.register(validate_input)
 
+def aaa(position, xCord, yCord):
+    createWindowForPlus(position, xCord, yCord)
+
 # UI components
 x1Label = Label(firstCoordsFrame, text = 'X1:')
 y1Label = Label(firstCoordsFrame, text = 'Y1:')
@@ -70,6 +156,7 @@ x1Coord = StringVar(value=1)
 y1Coord = StringVar(value=1)
 x1Entry = Entry(firstCoordsFrame, textvariable = x1Coord, width=6, validate="key", validatecommand=(validate_func, "%P"))
 y1Entry = Entry(firstCoordsFrame, textvariable = y1Coord, width=6, validate="key", validatecommand=(validate_func, "%P"))
+
 
 x2Label = Label(secondCoordsFrame, text = 'X2:')
 y2Label = Label(secondCoordsFrame, text = 'Y2:')
@@ -79,9 +166,13 @@ x2Entry = Entry(secondCoordsFrame, textvariable = x2Coord, width=6, validate="ke
 y2Entry = Entry(secondCoordsFrame, textvariable = y2Coord, width=6, validate="key", validatecommand=(validate_func, "%P"))
 
 liveCoords = Label(miscFrame, text = 'X: 0, Y: 0', width=12)
-sub_btn=Button(miscFrame,text = 'Snap', command = submit)
+snapBtn=Button(miscFrame, text = 'Snap', command = snapClicked)
 
-x1, x2, y1, y2 = 1, 1, 500, 500
+# Tracking user input
+x1Coord.trace_add("write", lambda var, index, mode:  aaa('start', x1Coord, y1Coord))
+y1Coord.trace_add("write", lambda *args:  aaa('start', x1Coord, y1Coord))
+x2Coord.trace_add("write", lambda *args:  aaa('end', x2Coord, y2Coord))
+y2Coord.trace_add("write", lambda *args:  aaa('end', x2Coord, y2Coord))
 
 # Organize frames
 firstCoordsFrame.pack(side=TOP)
@@ -98,8 +189,16 @@ x2Entry.pack(side=LEFT)
 y2Label.pack(side=LEFT)
 y2Entry.pack(side=LEFT)
 
-sub_btn.pack(side=RIGHT)
+snapBtn.pack(side=RIGHT)
 liveCoords.pack()
 
-update_coords()
-mainloop()
+updateCoords()
+root.bind("<F1>", setStartingCoords)
+root.bind("<F2>", setEndingCoords)
+root.bind("<F3>", snapClicked)
+root.bind("<Escape>", endApplication)
+
+try:
+    mainloop()
+except KeyboardInterrupt:
+    print("Interupted")
